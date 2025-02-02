b5594288955569832c1e4952f2150a42b58fd09f
Kyu <heyimkyu@mailbox.org>
2025-02-02 14:42:58 +0100

    now works with hovered or selected files

--- init.lua
+++ init.lua
@@ -1,28 +1,28 @@
---- @sync entry
+local selected_or_hovered = ya.sync(function()
+  local tab, paths = cx.active, {}
+  for _, u in pairs(tab.selected) do
+    paths[#paths + 1] = tostring(u)
+  end
+  if #paths == 0 and tab.current.hovered then
+    paths[1] = tostring(tab.current.hovered.url)
+  end
+  return paths
+end)
 
 local function extract_zstd(url)
-  if url:match("%.tar%.zst$") then
+  if url:match("%.tar.zst$") then
     os.execute("unzstd -fc " .. ya.quote(url) .. " | tar -xvf - " .. " >/dev/null 2>&1")
   else
     os.execute("unzstd -f " .. ya.quote(url) .. " >/dev/null 2>&1")
   end
 end
 
-local function is_zstd(mime)
-  if string.match(mime, "application/zstd") then
-    return true
-  end
-  return false
-end
-
 return {
   entry = function()
-    local h = cx.active.current.hovered
-    if h == nil then
-      return
+    for i, url in pairs(selected_or_hovered()) do
+      if url:match("%.zst$") then
+        extract_zstd(url)
+      end
     end
-    if is_zstd(h:mime()) then
-      local path = extract_zstd(tostring(h.url))
-    end
-  end,
+  end
 }
